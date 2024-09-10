4d70a6fa3c3d991c3d0aae52eb89a79c9f1a020c
bugra <bugrakaya020@gmail.com>
2024-09-10 19:26:03 +0300

    advaanced search

--- main.c
+++ main.c
@@ -70,7 +70,7 @@ void editorSetStatusMessage(const char *fmt, ...);
 
 void editorRefreshScreen();
 
-char *editorPrompt(char *prompt);
+char *editorPrompt(char *prompt, void (*callback)(char *, int));
 
 /* TERMINAL */
 
@@ -368,8 +368,9 @@ void editorOpen(char *filename){
 
 void editorSave() {
     if (E.filename == NULL){
-        E.filename = editorPrompt("Save as: %s (ESC to cancel)");
-        if (E.filename == NULL) {
+            E.filename = editorPrompt("Save as: %s (ESC to cancel)", NULL);
+            
+            if (E.filename == NULL) {
             editorSetStatusMessage("Save aborted");
             return;
         }
@@ -397,11 +398,11 @@ void editorSave() {
 
 /* SEARCH */
 
-void editorFind() {
-    char *query = editorPrompt("Search: %s (ESC to cancel)");
-    
-    if (query == NULL) return;
-    
+void editorFindCallback(char *query, int key) {
+    if (key == '\r' || key == '\x1b') {
+        return;
+    }
+
     int i;
     for (i = 0; i < E.numrows; i++) {
         erow *row = &E.row[i];
@@ -409,12 +410,19 @@ void editorFind() {
         
         if (match) {
             E.cy = i;
-            E.cx = editorRowRxToCx(row, match - row->render) + LINE_NUMBER_MARGIN;
+            E.cx = editorRowRxToCx(row, match - row->render);
             E.rowoff = E.numrows;
             break;
         }
     }
-    free(query);
+}
+
+void editorFind() {
+    char *query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
+    
+    if (query) {
+        free(query);
+    }
 }
 
 /* APPEND BUUFFER */
@@ -580,7 +588,7 @@ void editorSetStatusMessage(const char *fmt, ...) {
 
 /* INPUT */
 
-char *editorPrompt(char *prompt) {
+char *editorPrompt(char *prompt, void (*callback)(char *, int)){
     size_t bufsize = 128;
     char *buf = malloc(bufsize);
     size_t buflen = 0;
@@ -595,11 +603,13 @@ char *editorPrompt(char *prompt) {
             if (buflen != 0) buf[--buflen] = '\0';
         } else if (c == '\x1b') {
             editorSetStatusMessage("");
+            if (callback) callback(buf, c);
             free(buf);
             return NULL;
         } else if (c == '\r') {
             if (buflen != 0) {
                 editorSetStatusMessage("");
+                if (callback) callback(buf, c);
                 return buf;
             }
         } else if (!iscntrl(c) && c < 128) {
@@ -610,6 +620,8 @@ char *editorPrompt(char *prompt) {
             buf[buflen++] = c;
             buf[buflen] = '\0';
         }
+        
+        if (callback) callback(buf, c);
     }
 }
 
